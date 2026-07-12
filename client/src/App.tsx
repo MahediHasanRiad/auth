da1ca1cd1dc43dd3c54b61539c91295064e70c21
riad <contact.mahedihasan@gmail.com>
2026-07-12 08:09:25 +0600

    Implement Google authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import google from "./assets/google.png";
+import { GoogleLogin } from "@react-oauth/google";
+import { useGoogleOneTapLogin } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
+
 
 interface LoginButtonType {
   img: string;
   text: string;
+  getGoogleToken?: () => void
 }
 
-function LoginButton({ img, text }: LoginButtonType) {
+function LoginButton({ img, text, getGoogleToken }: LoginButtonType) {
   return (
-    <div className="flex justify-center items-center bg-white w-100 h-50 rounded-md ">
+    <div className="flex justify-center items-center bg-white w-100 h-50 rounded-md " onClick={getGoogleToken}>
       <button className="flex gap-4 items-center border rounded-md p-4 border-gray-300 cursor-pointer">
         <img src={img} alt="" className="w-8 h-8" />
         {text}
@@ -17,10 +22,49 @@ function LoginButton({ img, text }: LoginButtonType) {
 }
 
 function App() {
+
+
+  // if the user already logedIn use this email, then popup a dialog to Continue as this email
+  useGoogleOneTapLogin({
+    onSuccess: credentialResponse => {
+      console.log(credentialResponse);
+    },
+    onError: () => {
+      console.log('Login Failed');
+    },
+  });
+
+  // custom google login button
+  const login = useGoogleLogin({
+    onSuccess: tokenResponse => console.log(tokenResponse),
+  });
+
+  <GoogleLogin
+    onSuccess={credentialResponse => {
+      console.log(credentialResponse);
+    }}
+    onError={() => {
+      console.log('Login Failed');
+    }}
+    useOneTap
+  />;
+
   return (
     <section className="w-full flex justify-center items-center min-h-screen bg-gray-200">
-      {/* google login  */}
-      <LoginButton img={google} text="Sign in with Google" />
+
+      {/* google login */}
+      <LoginButton img={google} text="Custom Sign in with Google" getGoogleToken={login} />
+
+      {/* google official button  */}
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+        useOneTap
+      />
     </section>
   );
 }
